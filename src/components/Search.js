c9fda84be7f0735b8c2a6a40af77e4e2cc909ed9
Vivek Chauhan05 <vivekchauhan0395@gmail.com>
2023-01-21 15:50:06 +0530

    files added

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+const Search = () => {
+    return ( <
+        header > < div > <
+        h2 className = "header__title" > Search it.Explore it.Buy it. < /h2> <
+        input type = "text"
+        className = "header__search"
+        placeholder = "Enter an address, neighborhood, city, or ZIP code" /
+        >
+        <
+        /div> < /
+        header >
+    );
+}
+
+export default Search;
